eacc9d6b5972b9d24233a8b33d3e32fd45e44d0e
Maksim K <me@iknski.ru>
2023-08-30 22:26:41 +0300

    riched

--- hash.py
+++ hash.py
@@ -2,10 +2,12 @@ import hashlib
 from os import system
 from os.path import basename, dirname, splitext, isfile
 from sys import argv
-from colorama import init, Fore
+from rich import box
+from rich.console import Console
+from rich.table import Table
 
 
-init(autoreset=True)
+console = Console()
 
 
 def hash_file(hashfunc, filepath):
@@ -23,34 +25,29 @@ path_to_file = splitext(dirname(argv[2]))[0]
 
 # Проверка правильности ввода пути файла
 if isfile(argv[2]) is False:
-    print("=================================================================")
-    print(
-        Fore.RED
-        + f"Файл {Fore.YELLOW + argv[2] + Fore.RESET}{Fore.RED} не найден. Попробуйте снова!!!"
-    )
-    print(
-        Fore.RED
-        + f"File {Fore.YELLOW + argv[2] + Fore.RESET}{Fore.RED} not found. Try again!!!"
-    )
-    print("=================================================================")
+    console.print(
+        f"+--------------------------------------------------------------------+\n"
+        f"Файл * [u][yellow]{argv[2]}[/yellow][/u] * не найден. Попробуйте снова!!!\n"
+        f"File * [u][yellow]{argv[2]}[/yellow][/u] * not found. Try again!!!\n"
+        f"+--------------------------------------------------------------------+",
+        style="red")
     system("pause")
 
 # Проверка правильности ввода алгоритма хэширования
 elif argv[1] not in hashlib.algorithms_available:
-    print("=================================================================")
-    print(
-        Fore.RED
-        + f"Неподдерживаемый алгоритм {Fore.YELLOW + argv[1] + Fore.RESET}{Fore.RED}. Попробуйте снова!!!"
-    )
-    print(
-        Fore.RED
-        + f"Unsupported hash algorithm {Fore.YELLOW + argv[1] + Fore.RESET}{Fore.RED}. Try again!!!"
-    )
-    print("-----------------------------------------------------------------")
-    print("Поддерживаемые алгоритмы:")
-    print("Suported algorithms:")
-    print(Fore.GREEN + f"{hashlib.algorithms_available}")
-    print("=================================================================")
+    hash_algs = ", ".join(list(hashlib.algorithms_available))
+    console.print(
+        f"+--------------------------------------------------------------------+\n"
+        f"Неподдерживаемый алгоритм * [yellow]{argv[1]}[/yellow] *. Попробуйте снова!!!\n"
+        f"Unsupported hash algorithm * [yellow]{argv[1]}[/yellow] *. Try again!!!\n"
+        f"+--------------------------------------------------------------------+",
+    style="red")
+
+    # табличный вывод
+    table = Table(box=box.ASCII2,header_style="green", style="green", width=70)
+    table.add_column(f"Поддерживаемые алгоритмы\nSuported algorithms", justify="center", style="blue", no_wrap=False)
+    table.add_row(f"{hash_algs}")
+    console.print(table)
     system("pause")
 
 else:
@@ -59,48 +56,38 @@ else:
     # Если хэш-файл уже есть, проверить его валидность
     if isfile(f"{path_to_file}\{file_name}.{argv[1]}") is True:
         with open(
-            f"{path_to_file}\{file_name}.{argv[1]}", mode="r", encoding="UTF-8"
+                f"{path_to_file}\{file_name}.{argv[1]}", mode="r", encoding="UTF-8"
         ) as hfile:
             line = hfile.readline()
             if line == output:
-                print(
-                    "================================================================="
-                )
-                print(
-                    f"Файл {Fore.YELLOW + argv[2] + Fore.RESET} проверен.{Fore.GREEN} Хэш сумма совпадает!!!"
-                )
-                print(
-                    f"File {Fore.YELLOW + argv[2] + Fore.RESET} checked.{Fore.GREEN} Hash sum is match!!!"
-                )
-                print(
-                    "================================================================="
-                )
+                console.print(
+                    f"+--------------------------------------------------------------------+\n"
+                    f"Файл * [yellow]{argv[2]}[/yellow] * проверен. Хэш сумма совпадает!!!\n"
+                    f"File * [yellow]{argv[2]}[/yellow] * checked. Hash sum is match!!!\n"
+                    f"+--------------------------------------------------------------------+",
+                style="green")
                 system("pause")
+
             elif line != output:
-                print(
-                    "================================================================="
-                )
-                print(
-                    f"Файл {Fore.YELLOW + argv[2] + Fore.RESET} проверен.{Fore.RED} Хэш сумма не совпадает!!!"
-                )
-                print(
-                    f"File {Fore.YELLOW + argv[2] + Fore.RESET} checked.{Fore.RED} Hash sum mismatch!!!"
-                )
-                print(
-                    "================================================================="
-                )
+                console.print(
+                    f"+--------------------------------------------------------------------+\n"
+                    f"Файл * [yellow]{argv[2]}[/yellow] * проверен. Хэш сумма не совпадает!!!\n"
+                    f"File * [yellow]{argv[2]}[/yellow] * checked. Hash sum mismatch!!!\n"
+                    f"+--------------------------------------------------------------------+",
+                style="red")
                 system("pause")
 
     # Если хэш-файла нет, создать его
     else:
-        with open(
-            f"{path_to_file}\{file_name}.{argv[1]}", mode="w", encoding="UTF-8"
-        ) as hfile:
+        with open(f"{path_to_file}\{file_name}.{argv[1]}", mode="w", encoding="UTF-8") as hfile:
             hfile.write(output)
-        print("=================================================================")
-        print(Fore.YELLOW + output)
-        print("-----------------------------------------------------------------")
-        print(f"Хэш файл сохранен в: {Fore.GREEN}{path_to_file}\{file_name}.{argv[1]}")
-        print(f"Hash file saved to: {Fore.GREEN}{path_to_file}\{file_name}.{argv[1]}")
-        print("=================================================================")
+        table = Table(box=box.ASCII2,header_style="green", style="green", width=70)
+        table.add_column(f"{argv[1]}sum", justify="center", style="blue", no_wrap=False)
+        table.add_row(f"{output}")
+        console.print(table)
+        console.print(
+            f"Хэш файл сохранен в: * [u][yellow]{path_to_file}\{file_name}.{argv[1]}[/yellow][/u] *\n"
+            f"Hash file saved to: * [u][yellow]{path_to_file}\{file_name}.{argv[1]}[/yellow][/u] *\n"
+            f"+--------------------------------------------------------------------+",
+            style="green")
         system("pause")
